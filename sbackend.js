7621c44aee9df0e85e12409c7a59b4033ac9c99f
sezan88 <sjnrahman88@gmail.com>
2025-03-09 23:55:50 +0600

    first commit

---
+++ sbackend.js
@@ -0,0 +1,8 @@
+let text_box = document.getElementById("input-from-user");
+let display_wordCount = document.getElementById("total_words");
+
+text_box.addEventListener("input", function () {
+  let text = text_box.value.trim();
+  let wordCount = text === "" ? 0 : text.split(/\s+/).length;
+  display_wordCount.textContent = "Word Count: " + wordCount;
+});
