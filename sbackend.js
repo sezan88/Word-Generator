2400ccf700be5be6a939a0a3b3f9e3a114b007c4
sezan88 <sjnrahman88@gmail.com>
2025-03-13 13:49:42 +0600

    removed extra wordcount thing

--- sbackend.js
+++ sbackend.js
@@ -4,5 +4,5 @@ let display_wordCount = document.getElementById("total_words");
 text_box.addEventListener("input", function () {
   let text = text_box.value.trim();
   let wordCount = text === "" ? 0 : text.split(/\s+/).length;
-  display_wordCount.textContent = "Word Count: " + wordCount;
+  display_wordCount.textContent =  wordCount;
 });
